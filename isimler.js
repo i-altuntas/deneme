2d8a4e075dba208f99ce66e6abd720f4a19caccf
faruk mete <i.altuntas011@gmail.com>
2022-12-17 02:05:21 +0300

    ...

---
+++ isimler.js
@@ -0,0 +1,6 @@
+const data=[
+    {"adi":"faruk","soyadi":"altuntas","yasi":"32"},
+    {"adi":"sena","soyadi":"altuntas","yasi":"31"},
+    {"adi":"tahir","soyadi":"altuntas","yasi":"4"},
+    
+]
---
+++ src/isimler.js
@@ -0,0 +1,7 @@
+export const Veri=[
+    {"id":1,"adi":"faruk","soyadi":"altuntas","yasi":"32"},
+    {"id":2,"adi":"sena","soyadi":"altuntas","yasi":"31"},
+    {"id":3,"adi":"tahir","soyadi":"altuntas","yasi":"4"},
+    {"id":4,"adi":"ali","soyadi":"altuntas","yasi":"25"}
+    
+]
